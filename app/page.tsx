1798d05df8bdc2191362997c4ef3c082de7e6369
Chung Wei <chungwei@whiteroom.work>
2024-02-26 16:09:26 +0800

    Init commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,24 @@
+import Link from "next/link";
+import { redirect } from "next/navigation";
+
 /** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  return null;
+export default function Home({ searchParams }: { searchParams: any }) {
+  return (
+    <div>
+      <p>Current Search Params:</p>
+      <pre>{JSON.stringify(searchParams, null, 2)}</pre>
+      <hr />
+      <form
+        action={async () => {
+          "use server";
+          redirect("/?foo=bar"); // <- This works in 14.1, not 14.1.1
+        }}
+      >
+        <button>Add some search params</button>
+      </form>
+
+      {/* Clearing search params with Link doesn't work too in 14.1.1 */}
+      <Link href="/">Clear search params</Link>
+    </div>
+  );
 }
